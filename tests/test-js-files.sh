1aee33573d519cd600c21c026bd4f62204ab442d
fuxiaohei <fuxiaohei@vip.qq.com>
2024-08-12 10:20:40 +0800

    refactor: js files tests move to runtime-land-js

--- tests/test-js-files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CLI=./target/release/land-cli
-
-# iterate tests/js-files/*.js
-for file in tests/js-files/*.js; do
-    echo "Building $file"
-    $CLI build $file --verbose
-done
