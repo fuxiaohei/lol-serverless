2c3e0a81d344f815dd36fc0e1bf83edb3ed7e480
fuxiaohei <fuxiaohei@vip.qq.com>
2024-10-26 23:34:09 +0800

    crates: add helpers crate, server: init cli

--- land-server/src/main.rs
+++ land-server/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+#[clap(disable_version_flag = true)] // handled manually
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    about = concat!(env!("CARGO_PKG_NAME")," ",env!("CARGO_PKG_VERSION")),
+)]
+struct Args {
+    /// Print version info and exit.
+    #[clap(short = 'V', long)]
+    version: bool,
+    /// Verbose mode.
+    #[clap(flatten)]
+    output: land_helpers::logging::TraceArgs,
+    /// Address to listen on.
+    #[clap(long, default_value("0.0.0.0:9840"))]
+    address: String,
+    /// Template directory
+    #[clap(long)]
+    tpldir: Option<String>,
+    // Database connection args.
+    //#[clap(flatten)]
+    //dbargs: land_dao::DBArgs,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    if args.version {
+        land_helpers::version::println(env!("CARGO_PKG_NAME"), args.output.verbose);
+        return Ok(());
+    }
+
+    Ok(())
 }
